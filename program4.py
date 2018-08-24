d8ff9331d04bf9cd9a031fd75ca1f1747c316735
nikola-cz <nikola.czerwik@gmail.com>
2018-08-24 12:50:33 +0200

    first

---
+++ program4.py
@@ -0,0 +1,9 @@
+name = input('Podaj nazwę języka programowania: ')
+if name == 'python':
+    print('To jest python')
+elif name == 'java':
+    print('To jest java')
+elif name == 'php':
+    print('To jest php')
+else:
+    print('Nie znam tego języka')    
